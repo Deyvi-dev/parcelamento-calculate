43a9c65e0695ccded45b33c8cc5acdb1ca15f5f3
Deyvisson <68565306+Deyvi-dev@users.noreply.github.com>
2023-04-23 21:17:31 -0300

    Update parcela.js

--- parcela.js
+++ parcela.js
@@ -1,17 +1,15 @@
- var total = 100.50 // valor da camisa em reais
-  2 var parcela = 3; // número de parcelas
-  3 
-  4 var valorParcela = Math.floor(total / parcela); // divisão arredondada para     baixo
-  5 var valorTotalDasParcelas = valorParcela * parcela;
-  6 console.log(valorTotalDasParcelas)
-  7 if (total === valorTotalDasParcelas) {
-  8     console.log(valorParcela)
-  9   console.log("OK");
- 10 } else {
- 11   var diferenca = total - valorTotalDasParcelas;
- 12   var valorUltimaParcela = valorParcela + diferenca;
- 13   console.log("O valor das duas primeiras parcelas é: " + valorParcela);
- 14   console.log("O valor da última parcela é: " + valorUltimaParcela);
- 15 }
- 16 
+let total = 100.50 // valor do produto total
+let parcela = 3; // número de parcelas
+let valorParcela = Math.floor(total / parcela); // divisão arredondada para baixo
 
+let valorTotalDasParcelas = valorParcela * parcela;
+  console.log(valorTotalDasParcelas)
+  if (total === valorTotalDasParcelas) {
+     console.log(valorParcela)
+     console.log("OK");
+ } else {
+    diferenca = total - valorTotalDasParcelas;
+    let valorUltimaParcela = valorParcela + diferenca;
+   console.log("O valor das duas primeiras parcelas é: " + valorParcela);
+   console.log("O valor da última parcela é: " + valorUltimaParcela);
+  }
